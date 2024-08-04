2dff873ab2dd90158f3a483302d3f7c26f5d2e63
Pavan BG <gpbabc@yahoo.com>
2024-08-04 20:59:43 +0530

    Implemented bounds and updated sign convention

--- splitnewton/newton.py
+++ splitnewton/newton.py
@@ -9,8 +9,18 @@ def criterion(x, s, abs=1e-5, rel=1e-6):
     return la.norm(s/(x*rel + abs))
 
 
+def check_within_bounds(x0, bounds):
+    if bounds is None:
+        return True
 
-def newton(df, J, x0, maxiter=Inf, sparse=False, dt0=0., dtmax=1., armijo=False):
+    lower, upper = bounds
+    for i in range(len(x0)):
+        if not (lower[i] <= x0[i] <= upper[i]):
+            return False
+    return True
+
+
+def newton(df, J, x0, maxiter=Inf, sparse=False, dt0=0., dtmax=1., armijo=False, bounds=None, bound_fac=0.8):
     """
     Unbounded Newton with pseudo-transient continuation
     (SER criterion for timestep modification) and Armijo rule
@@ -28,6 +38,8 @@ def newton(df, J, x0, maxiter=Inf, sparse=False, dt0=0., dtmax=1., armijo=False)
         dt0: float, Initial pseudo-timestep size
         dtmax: float, Maximum pseudo-timestep size
         armijo: bool, Apply Armijo rule to choose step fraction
+        bounds: list of 2-lists, each of size len(x) that contain lower and upper bound
+        bound_fac: damping factor by which solver must step to avoid crossing limits
 
     Returns
     -------
@@ -39,6 +51,10 @@ def newton(df, J, x0, maxiter=Inf, sparse=False, dt0=0., dtmax=1., armijo=False)
     if dt0 < 0 or dtmax < 0:
         raise Exception("Must specify positive dt0 and dtmax")
     dt = dt0
+
+    # Check if seed within bounds
+    if not check_within_bounds(x0, bounds):
+        raise Exception("Seed must be within the provided bounds")
 
     x = deepcopy(x0)
     f0 = la.norm(df(x0))
@@ -57,12 +73,16 @@ def newton(df, J, x0, maxiter=Inf, sparse=False, dt0=0., dtmax=1., armijo=False)
         fn = la.norm(dfx)
 
         # Choose solver based on sparsity
+        # Note that the final step is -s
         if sparse:
             s, info = sp.linalg.gmres(sp.csr_matrix(jac), dfx)
             if info != 0:
                 logging.warning("GMRES not converged")
         else:
             s = la.solve(jac, dfx)
+
+        # Apply sign convention
+        s = -s
 
         # Armijo rule
         if armijo:
@@ -76,6 +96,41 @@ def newton(df, J, x0, maxiter=Inf, sparse=False, dt0=0., dtmax=1., armijo=False)
             s *= fac
             logging.info(f"Armijo factor: {fac}")
 
+        ####
+        # Apply bounds
+        ####
+        if bounds is not None:
+            # Check if valid format of bounds
+            if len(bounds) != 2:
+                raise Exception(
+                    "Bounds must be a 2 lists, with lower and upper bounds of each dimension")
+            lower, upper = bounds
+            if len(lower) != len(x) or len(upper) != len(x):
+                raise Exception(
+                    "Each bounds list must be as long as the solution vector")
+
+            # Set the scaling factor as the highest damping that must be done
+            best_scaling = 1.0
+            for i in range(len(x)):
+                if s[i] == 0.0:
+                    continue
+                # Move only atmost `legal_delta` in each dimension
+                else:
+                    # Calculate the new position and legal delta
+                    legal_delta = bound_fac * \
+                        (lower[i] - x[i]) if s[i] < 0 else bound_fac * \
+                        (upper[i] - x[i])
+                    # Both legal_delta and s[i] will be of the same sign
+                    # If it exceeds 1, the step is retained, else
+                    # the scaling factor is updated
+                    scaling = legal_delta / s[i]
+                    best_scaling = min(best_scaling, scaling)
+
+            # Show damping factor if invoked
+            if best_scaling != 1.0:
+                logging.info(f"Bounds hit. Damping factor: {best_scaling}")
+            s *= best_scaling
+
         # Check convergence
         crit = criterion(x, s)
         logging.info(f"{x}, {s}, {crit}")
@@ -83,11 +138,11 @@ def newton(df, J, x0, maxiter=Inf, sparse=False, dt0=0., dtmax=1., armijo=False)
             logging.info(f"Timestep: {dt}")
 
         # Update x
-        x -= s
+        x += s
         iter += 1
 
         # Update timestep
         # No if condition as much faster directly
         dt = min(dt0*f0/fn, dtmax)
-        
+
     return x, s, iter
--- splitnewton/split_newton.py
+++ splitnewton/split_newton.py
@@ -8,7 +8,9 @@ def attach(x, y):
     return concatenate([x, y])
 
 # Specify non-zero dt0 for pseudo-transient continuation
-def split_newton(df, J, x0, loc, maxiter=Inf, sparse=False, dt0=0, dtmax=1., armijo=False):
+
+
+def split_newton(df, J, x0, loc, maxiter=Inf, sparse=False, dt0=0, dtmax=1., armijo=False, bounds=None, bound_fac=0.8):
     """
     Unbounded SPLIT Newton with pseudo-transient continuation
     (SER criterion for timestep modification) and Armijo rule
@@ -27,6 +29,8 @@ def split_newton(df, J, x0, loc, maxiter=Inf, sparse=False, dt0=0, dtmax=1., arm
         dt0: float, Initial pseudo-timestep size
         dtmax: float, Maximum pseudo-timestep size
         armijo: bool, Apply Armijo rule to choose step fraction
+        bounds: list of 2-lists, each of size len(x) that contain lower and upper bound
+        bound_fac: damping factor by which solver must step to avoid crossing limits
 
     Returns
     -------
@@ -50,32 +54,37 @@ def split_newton(df, J, x0, loc, maxiter=Inf, sparse=False, dt0=0, dtmax=1., arm
     crit = Inf
 
     iter = 0
-    while crit >= 1  and iter < maxiter:
+    while crit >= 1 and iter < maxiter:
         # B Cycle
-        dfb = lambda x: df(attach(xa, x))[loc:]
-        Jb = lambda x: J(attach(xa, x))[loc:, loc:]
-        xb, sb, local_iter = newton(dfb, Jb, xb, maxiter, sparse, dt, dtmax, armijo)
+        def dfb(x): return df(attach(xa, x))[loc:]
+        def Jb(x): return J(attach(xa, x))[loc:, loc:]
+        local_bounds = [bounds[0][loc:], bounds[1]
+                        [loc:]] if bounds is not None else None
+        xb, sb, local_iter = newton(
+            dfb, Jb, xb, maxiter, sparse, dt, dtmax, armijo, local_bounds)
         logging.debug(f"B cycle: {xb}, {sb}")
         logging.debug(f"B iterations: {local_iter}")
 
         # A Cycle
-        dfa = lambda x: df(attach(x, xb))[:loc]
-        Ja = lambda x: J(attach(x, xb))[:loc, :loc]
-        xa, sa, local_iter = newton(dfa, Ja, xa, 1, sparse, dt, dtmax, armijo)
+        def dfa(x): return df(attach(x, xb))[:loc]
+        def Ja(x): return J(attach(x, xb))[:loc, :loc]
+        local_bounds = [bounds[0][:loc], bounds[1]
+                        [:loc]] if bounds is not None else None
+        xa, sa, local_iter = newton(
+            dfa, Ja, xa, 1, sparse, dt, dtmax, armijo, local_bounds)
         logging.debug(f"A cycle: {xa}, {sa}")
         logging.debug(f"A iterations: {local_iter}")
 
         # Construct new x and step
         xnew = attach(xa, xb)
         s = xnew - x
-        
+
         # Check convergence
         crit = criterion(x, s)
         logging.info(f"{x}, {s}, {crit}")
-        
+
         # Update x
         x = xnew
         iter += 1
 
     return x, s, iter
-    
