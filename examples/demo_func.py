3feb83457a3aa53511a12571f151fc2cdc3829da
Pavan BG <gpbabc@yahoo.com>
2024-08-04 23:45:26 +0530

    Basic testing for Newton added

--- examples/demo_func.py
+++ examples/demo_func.py
@@ -5,6 +5,7 @@ lambda_b = 2
 lambda_c = -1
 lambda_d = -4
 
+
 def common_init(x0):
     la = int(len(x0)/2)
     lb = len(x0) - la
@@ -14,17 +15,19 @@ def common_init(x0):
 
     return a, b
 
+
 def test_func(x0):
     a, b = common_init(x0)
     coeff = np.concatenate((a, b))
     return 0.25 * sum(coeff * (np.array(x0) ** 4))
 
+
 def test_der(x0):
     a, b = common_init(x0)
     return np.concatenate((a, b)) * (np.array(x0) ** 3)
+
 
 def test_hess(x0):
     a, b = common_init(x0)
     diagonal = 3 * np.concatenate((a, b)) * (np.array(x0) ** 2)
     return np.diag(diagonal)
-
---
+++ tests/test_newton.py
@@ -0,0 +1,127 @@
+import pytest
+import numpy as np
+import numpy.linalg as la
+from scipy.optimize import rosen, rosen_der, rosen_hess
+from splitnewton.newton import criterion, check_within_bounds, newton
+
+# Tests for criterion function
+
+
+def test_criterion():
+    x = np.array([1.0, 2.0])
+    s = np.array([0.1, 0.2])
+    result = criterion(x, s)
+    expected = la.norm(s / (x * 1e-6 + 1e-5))
+    assert np.isclose(result, expected)
+
+# Tests for check_within_bounds function
+
+
+def test_check_within_bounds_valid():
+    x0 = [1.0, 2.0, 3.0]
+    bounds = [[0.0, 1.5, 2.5], [2.0, 3.0, 4.0]]
+    assert check_within_bounds(x0, bounds)
+
+
+def test_check_within_bounds_invalid():
+    x0 = [1.0, 2.0, 5.0]
+    bounds = [[0.0, 1.5, 2.5], [2.0, 3.0, 4.0]]
+    assert not check_within_bounds(x0, bounds)
+
+
+def test_check_within_bounds_no_bounds():
+    x0 = [1.0, 2.0]
+    bounds = None
+    assert check_within_bounds(x0, bounds)
+
+
+def test_check_within_bounds_bounds_format():
+    with pytest.raises(Exception, match="Bounds must be a 2 lists, with lower and upper bounds of each dimension"):
+        check_within_bounds([1.0], [[0.0, 1.0]])
+
+    with pytest.raises(Exception, match="Each bounds list must be as long as the solution vector"):
+        check_within_bounds([1.0, 2.0], [[0.0], [1.0]])
+
+# Tests for newton function
+
+
+def test_negative_dt0():
+    x0 = np.array([0.1, 0.2])
+    func, der, hess = rosen, rosen_der, rosen_hess
+    with pytest.raises(Exception, match="Must specify positive dt0 and dtmax"):
+        newton(der, hess, x0, dt0=-1.)
+
+
+def test_invalid_seed():
+    x0 = np.array([2., 2.])
+    bounds = [[0., 0.], [1., 1.]]
+    func, der, hess = rosen, rosen_der, rosen_hess
+    with pytest.raises(Exception, match="Seed must be within the provided bounds"):
+        newton(der, hess, x0, bounds=bounds)
+
+
+def test_newton_no_bounds():
+    x0 = np.array([0.1, 0.2])
+    x_expected = np.array([1.0, 1.0])
+    func, der, hess = rosen, rosen_der, rosen_hess
+    x, s, iter = newton(der, hess, x0)
+    assert np.allclose(x, x_expected, atol=1e-5)
+    assert np.allclose(0.0, rosen(x), atol=1e-5)
+
+
+def test_newton_with_bounds():
+    x0 = np.array([0.25, 0.2])
+    bounds = [[0., 0.], [2., 2.]]
+    x_expected = np.array([1.0, 1.0])
+    func, der, hess = rosen, rosen_der, rosen_hess
+    x, s, iter = newton(der, hess, x0, bounds=bounds)
+    assert np.allclose(x, x_expected, atol=1e-5)
+    assert np.allclose(0.0, rosen(x), atol=1e-5)
+
+
+def test_armijo_rule():
+    # Initial guess
+    x0 = np.array([0.0])
+
+    # Function values and gradients
+    def df(x):
+        return x - 1
+
+    def J(x):
+        return np.eye(len(x))
+
+    # Expected result is close to the optimal point which is x = 1
+    expected_x = np.array([1.0])
+    tolerance = 1e-6
+
+    # Run the Newton solver with Armijo rule
+    x_opt, step, iterations = newton(
+        df, J, x0, maxiter=1, sparse=True, armijo=True)
+
+    # Check if Armijo scaling is working
+    # The step should be scaled down, so check if the step is less than initial step size
+    initial_step_size = 1.0
+    assert np.linalg.norm(
+        step) < initial_step_size, "Armijo scaling did not reduce the step size"
+
+
+def test_newton_sparse_solver():
+    x0 = np.array([0.1, 0.2])
+    x_expected = np.array([1.0, 1.0])
+    func, der, hess = rosen, rosen_der, rosen_hess
+    x, s, iter = newton(der, hess, x0, sparse=True)
+    assert np.allclose(x, x_expected, atol=1e-5)
+    assert np.allclose(0.0, rosen(x), atol=1e-5)
+
+
+def test_newton_invalid_bounds():
+    x0 = np.array([0.0, 0.0])
+    bounds = [[0.0, 0.0], [1.0]]
+    func, der, hess = rosen, rosen_der, rosen_hess
+    with pytest.raises(Exception, match="Each bounds list must be as long as the solution vector"):
+        newton(der, hess, x0, maxiter=10, bounds=bounds)
+
+
+# Run the tests
+if __name__ == "__main__":
+    pytest.main()
