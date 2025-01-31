b958c6c92873defbb6b1028c8441886c5b335965
Pavan BG <gpbabc@yahoo.com>
2025-01-31 23:28:20 +0530

    Created 3-layer example

--- examples/demo_func.py
+++ examples/demo_func.py
@@ -4,30 +4,34 @@ lambda_a = 6
 lambda_b = 2
 lambda_c = -1
 lambda_d = -4
+lambda_e = -6
+lambda_f = -8
 
 
 def common_init(x0):
-    la = int(len(x0)/2)
-    lb = len(x0) - la
+    la = len(x0)//3
+    lb = len(x0)//3
+    lc = len(x0) - la - lb
 
     a = np.logspace(lambda_a, lambda_b, la)
     b = np.logspace(lambda_c, lambda_d, lb)
+    c = np.logspace(lambda_c, lambda_d, lc)
 
-    return a, b
+    return a, b, c
 
 
 def test_func(x0):
-    a, b = common_init(x0)
-    coeff = np.concatenate((a, b))
+    a, b, c = common_init(x0)
+    coeff = np.concatenate((a, b, c))
     return 0.25 * sum(coeff * (np.array(x0) ** 4))
 
 
 def test_der(x0):
-    a, b = common_init(x0)
-    return np.concatenate((a, b)) * (np.array(x0) ** 3)
+    a, b, c = common_init(x0)
+    return np.concatenate((a, b, c)) * (np.array(x0) ** 3)
 
 
 def test_hess(x0):
-    a, b = common_init(x0)
-    diagonal = 3 * np.concatenate((a, b)) * (np.array(x0) ** 2)
+    a, b, c = common_init(x0)
+    diagonal = 3 * np.concatenate((a, b, c)) * (np.array(x0) ** 2)
     return np.diag(diagonal)
--- examples/main.py
+++ examples/main.py
@@ -27,7 +27,7 @@ loglevel = getattr(logging, args.loglevel.upper())
 logging.basicConfig(level=loglevel)
 
 # Seed
-x0 = np.linspace(21.2, 31.2, 50)
+x0 = np.linspace(21.2, 31.2, 5000)
 
 mode = "TEST"  # or ROSENBROCK
 func, der, hess = set_functions(mode)
@@ -35,30 +35,15 @@ func, der, hess = set_functions(mode)
 dt0 = 0.0
 dtmax = 0.1
 
-# Split Newton
-start = time()
-print('Starting Split-Newton...')
-xfa, _, iter = split_newton(
-    der, hess, x0, [len(x0)//2], maxiter=100, sparse=True, dt0=dt0, dtmax=dtmax)
-print("Final root: ", xfa)
-print("Final Residual: ", func(xfa))
-print(f"Elapsed time: {time() - start}")
-print(f"Total iterations: {iter}")
-input('')
-
-print('-' * 20)
-
 # Hierarchical Split Newton
 start = time()
 print('Starting Hierarchical Split-Newton...')
 xfb, _, iter = split_newton(
-    der, hess, x0, [len(x0)//4, 3*len(x0)//4, 7*len(x0)//8], maxiter=100, sparse=True, dt0=dt0, dtmax=dtmax)
+    der, hess, x0, [len(x0)//3, 2*len(x0)//3], maxiter=100, sparse=True, dt0=dt0, dtmax=dtmax)
 print("Final root: ", xfb)
 print("Final Residual: ", func(xfb))
 print(f"Elapsed time: {time() - start}")
 print(f"Total iterations: {iter}")
-print("Split and Hierarchical agree/difference: ", "Yes" if np.allclose(
-    xfa, xfb, atol=1e-6) else np.max(np.abs(xfa - xfb)))
 input('')
 
 print('-' * 20)
@@ -72,6 +57,4 @@ print("Final root: ", xfc)
 print("Final Residual: ", func(xfc))
 print(f"Elapsed time: {time() - start}")
 print(f"Total iterations: {iter}")
-print("Split and Newton agree/difference: ", "Yes" if np.allclose(
-    xfa, xfc, atol=1e-6) else np.max(np.abs(xfa - xfc)))
 input('')
